a1a00a33290f6e2956b9647554cb130399fe7ca1
emil7720 <Baron.Emil.Exp@gmail.com>
2023-10-22 01:49:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Starts
+{
+    class Program
+    {
+        enum Suits
+        {
+            Wands,
+            Coins,
+            Cups,
+            Swords
+        }
+
+        public static void Main()
+        {
+            Console.WriteLine(GetSuit(Suits.Wands));
+            Console.WriteLine(GetSuit(Suits.Coins));
+            Console.WriteLine(GetSuit(Suits.Cups));
+            Console.WriteLine(GetSuit(Suits.Swords));
+        }
+        private static string GetSuit(Suits suit)
+        {
+            return new string[] { "Wands", "Coins", "Cups", "Swords" }[((int)suit)];
+        }
+    }
+}
